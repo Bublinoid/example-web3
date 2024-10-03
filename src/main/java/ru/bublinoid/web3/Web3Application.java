638efe9a40ff0d37487df995b19cbe12e4e3da80
Hleb <gleb.qb@gmail.com>
2024-10-03 18:32:49 +0200

    .

--- src/main/java/ru/bublinoid/web3/Web3Application.java
+++ src/main/java/ru/bublinoid/web3/Web3Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Web3Application {
     public static void main(String[] args) {
         SpringApplication.run(Web3Application.class, args);
-        //TODO
+
     }
 
 }
